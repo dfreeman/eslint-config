0b9150acd53067b74e7fdf6042394b3e1b1ab491
Dan Freeman <dfreeman@salsify.com>
2019-09-02 18:52:50 +0200

    Don't enforce TS rules in .js files

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
+/* eslint-env node */
 module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint/eslint-plugin', 'eslint-plugin-prettier'],
-  extends: ['plugin:@typescript-eslint/recommended', 'prettier/@typescript-eslint', 'plugin:prettier/recommended'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended'
+  ],
   rules: {
     '@typescript-eslint/explicit-function-return-type': ['warn', { allowExpressions: true }]
-  }
+  },
+  overrides: [
+    {
+      files: ['**/*.js'],
+      rules: Object.keys(require('@typescript-eslint/eslint-plugin/dist/configs/recommended').rules).reduce(
+        (rules, rule) => {
+          rules[rule] = 'off';
+          return rules;
+        },
+        {}
+      )
+    }
+  ]
 };
