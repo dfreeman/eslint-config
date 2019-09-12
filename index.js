f670f6f6b3e4ac4a9f6213eb484dbe6549cbfb7b
Dan Freeman <dfreeman@salsify.com>
2019-09-12 21:42:27 +0200

    Why isn't no-dupe-class-members disabled by default for TS files?

--- index.js
+++ index.js
@@ -1,28 +1,26 @@
 /* eslint-env node */
 module.exports = {
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint/eslint-plugin', 'eslint-plugin-prettier'],
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'prettier/@typescript-eslint',
-    'plugin:prettier/recommended',
-  ],
+  plugins: ['eslint-plugin-prettier'],
+  extends: ['eslint:recommended', 'plugin:prettier/recommended'],
   rules: {
     'prefer-const': 'off',
-    '@typescript-eslint/explicit-function-return-type': ['warn', { allowExpressions: true }],
-    '@typescript-eslint/no-use-before-define': 'off',
   },
   overrides: [
     {
-      files: ['**/*.js'],
-      rules: Object.keys(require('@typescript-eslint/eslint-plugin/dist/configs/recommended').rules).reduce(
-        (rules, rule) => {
-          rules[rule] = 'off';
-          return rules;
-        },
-        {}
-      ),
+      files: ['**/*.ts'],
+      parser: '@typescript-eslint/parser',
+      plugins: ['@typescript-eslint/eslint-plugin', 'eslint-plugin-prettier'],
+      extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'prettier/@typescript-eslint',
+        'plugin:prettier/recommended',
+      ],
+      rules: {
+        'no-dupe-class-members': 'off',
+        '@typescript-eslint/explicit-function-return-type': ['warn', { allowExpressions: true }],
+        '@typescript-eslint/no-use-before-define': 'off',
+      },
     },
     {
       // Typical project config files
