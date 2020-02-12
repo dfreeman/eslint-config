8a3134cf7751d9e073bcff9d1021184f7d719d09
Dan Freeman <dfreeman@salsify.com>
2020-02-12 08:33:44 -0700

    Moar rules

--- index.js
+++ index.js
@@ -17,9 +17,56 @@ module.exports = {
         'plugin:prettier/recommended',
       ],
       rules: {
+        'prefer-const': 'off',
         'no-dupe-class-members': 'off',
-        '@typescript-eslint/explicit-function-return-type': ['warn', { allowExpressions: true }],
+        '@typescript-eslint/explicit-function-return-type': ['error', { allowExpressions: true }],
         '@typescript-eslint/no-use-before-define': 'off',
+        '@typescript-eslint/array-type': ['error', { default: 'generic' }],
+
+        '@typescript-eslint/explicit-member-accessibility': 'error',
+
+        // Disallow the use of bare `Function`, `Object` and boxed types
+        '@typescript-eslint/ban-types': [
+          'error',
+          {
+            types: {
+              Function: {
+                fixWith: '() => unknown',
+                message: 'Avoid `Function` in favor of a more precise callable type.',
+              },
+
+              Object: {
+                fixWith: '{}',
+                message:
+                  'Use `{}` for objects with no specialized fields, or `object` if you really mean "any non-primitive value".',
+              },
+
+              String: {
+                fixWith: 'string',
+                message:
+                  'Avoid boxed types. See https://www.typescriptlang.org/docs/handbook/declaration-files/do-s-and-don-ts.html#general-types',
+              },
+
+              Number: {
+                message:
+                  'Avoid boxed types. See https://www.typescriptlang.org/docs/handbook/declaration-files/do-s-and-don-ts.html#general-types',
+                fixWith: 'number',
+              },
+
+              Symbol: {
+                message:
+                  'Avoid boxed types. See https://www.typescriptlang.org/docs/handbook/declaration-files/do-s-and-don-ts.html#general-types',
+                fixWith: 'symbol',
+              },
+
+              Boolean: {
+                message:
+                  'Avoid boxed types. See https://www.typescriptlang.org/docs/handbook/declaration-files/do-s-and-don-ts.html#general-types',
+                fixWith: 'boolean',
+              },
+            },
+          },
+        ],
       },
     },
     {
